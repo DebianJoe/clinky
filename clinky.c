3d88994bf11c4459b691e082eec5612b5aa82f14
DebianJoe <jbpaladin27@gmail.com>
2014-09-30 06:22:52 -0500

    updated sys pulls

--- clinky.c
+++ clinky.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
-
+#include <sys/sysinfo.h>
 
 #define WIDTH 30
 #define HEIGHT 10
 
+/* set conversion constants */
+const long minute = 60;
+const double megabyte = 1024 * 1024;
 int startx = 0;
 int starty = 0;
 char *labels[] = {  /* set up an array for things to track */
@@ -17,8 +20,11 @@ char *labels[] = {  /* set up an array for things to track */
 int main() {
     initscr();
     noecho();
+    struct sysinfo si;
+    sysinfo (&si);
 
     mvprintw(0, 0,"%s", labels[0]);
+    mvprintw(1, 0,"%5.1f", (si.totalram - si.freeram) / megabyte);
     mvprintw(0, 5,"%s", labels[1]);
     refresh();
     getch();
