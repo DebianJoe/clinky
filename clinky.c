ab8bf92a0a0e864188ea6d8891c73885a34d69cf
DebianJoe <jbpaladin27@gmail.com>
2014-10-01 03:36:46 -0500

    cache details

--- clinky.c
+++ clinky.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
-#include <ncurses.h>
 #include <stdlib.h>
 #include <sys/sysinfo.h>
-
-#define WIDTH 30
-#define HEIGHT 10
+#include <unistd.h>
+#include <math.h>
 
 /* set conversion constants */
-const long minute = 60;
 const double megabyte = 1024 * 1024;
-int startx = 0;
-int starty = 0;
-
-
-char *labels[] = {  /* set up an array for things to track */
-	"mem",
-	"cpu",
-    };
 
 int main() {
-    /* initscr(); */
-    /* noecho(); */
     struct sysinfo si;
     sysinfo (&si);
 
@@ -28,27 +15,28 @@ int main() {
     unsigned long usedmem = ((si.totalram *(unsigned long long)si.mem_unit) -
 			     (si.freeram *(unsigned long long)si.mem_unit)) / megabyte;
     unsigned long buffermem = (si.bufferram *(unsigned long long)si.mem_unit) / megabyte;
-
-
-    /* screen formatting, delayed until I get everything else working */
-    /* mvprintw(0, 0,"%s", labels[0]); */
-    /* mvprintw(1, 0,"%5.1f", */
-    /* 	     ((si.totalram - si.freeram) - si.bufferram) / megabyte); */
-    /* mvprintw(0, 5,"%s", labels[1]); */
-    /* mvprintw(1, 5,"%k", */
-    /* 	     (si.totalram *(unsigned long long)si.mem_unit / 1024)); */
-    /* refresh(); */
-    /* getch(); */
-    /* endwin(); */
-    /* noecho(); */
-
     /* raw readings */
     printf("Total Ram: %lluk\tFree: %lluk\n",
 	   si.totalram *(unsigned long long)si.mem_unit / 1024,
 	   si.freeram *(unsigned long long)si.mem_unit / 1024);
-    printf("Total: %lu in human form\n", totalmem);
-    printf("Used:  %lu in human form\n", usedmem);
-    printf("Buffer:%lu in human form\n", buffermem);
+    printf("Total: \033[1;31m%luM\033[1;m in human form\n", totalmem);
+    printf("Used:  %luM in human form\n", usedmem);
+    printf("Buffer:%luM in human form\n", buffermem);
+    long size = sysconf(_SC_LEVEL1_ICACHE_SIZE) / pow(2, 10);
+    long assoc = sysconf(_SC_LEVEL1_ICACHE_ASSOC);
+    long line = sysconf(_SC_LEVEL1_ICACHE_LINESIZE);
+    printf("lvl 1 icache size = %ldK, assoc = %ld, line size = %ld\n", size, assoc, line);
+
+    size = sysconf(_SC_LEVEL1_DCACHE_SIZE) / pow(2, 10);
+    assoc = sysconf(_SC_LEVEL1_DCACHE_ASSOC);
+    line = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+    printf("lvl 1 dcache size = %ldK, assoc = %ld, line size = %ld\n", size, assoc, line);
+
+    size = sysconf(_SC_LEVEL2_CACHE_SIZE) / pow(2, 10);
+    assoc = sysconf(_SC_LEVEL2_CACHE_ASSOC);
+    line = sysconf(_SC_LEVEL2_CACHE_LINESIZE);
+    printf("lvl 2 cache size = %ldK, assoc = %ld, line size = %ld\n", size, assoc, line);
+
 
     return 0;
 }
