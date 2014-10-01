05c51e040648e433180d1190ec589f00aba91a6f
DebianJoe <jbpaladin27@gmail.com>
2014-10-01 02:18:10 -0500

    broke curses formatting for troubleshooting figures

--- clinky.c
+++ clinky.c
@@ -11,25 +11,38 @@ const long minute = 60;
 const double megabyte = 1024 * 1024;
 int startx = 0;
 int starty = 0;
+
+
 char *labels[] = {  /* set up an array for things to track */
 	"mem",
 	"cpu",
     };
 
-
 int main() {
-    initscr();
-    noecho();
+    /* initscr(); */
+    /* noecho(); */
     struct sysinfo si;
     sysinfo (&si);
 
-    mvprintw(0, 0,"%s", labels[0]);
-    mvprintw(1, 0,"%5.1f", (si.totalram - si.freeram) / megabyte);
-    mvprintw(0, 5,"%s", labels[1]);
-    refresh();
-    getch();
-    endwin();
-    noecho();
+    unsigned long totalmem = si.totalram *(unsigned long long)si.mem_unit / megabyte;
+    unsigned long usedmem = ((si.totalram *(unsigned long long)si.mem_unit) -
+			     (si.freeram *(unsigned long long)si.mem_unit)) / megabyte;
+    /* screen formatting */
+    /* mvprintw(0, 0,"%s", labels[0]); */
+    /* mvprintw(1, 0,"%5.1f", */
+    /* 	     ((si.totalram - si.freeram) - si.bufferram) / megabyte); */
+    /* mvprintw(0, 5,"%s", labels[1]); */
+    /* mvprintw(1, 5,"%k", */
+    /* 	     (si.totalram *(unsigned long long)si.mem_unit / 1024)); */
+    /* refresh(); */
+    /* getch(); */
+    /* endwin(); */
+    /* noecho(); */
+    printf("Total Ram: %lluk\tFree: %lluk\n",
+	   si.totalram *(unsigned long long)si.mem_unit / 1024,
+	   si.freeram *(unsigned long long)si.mem_unit / 1024);
+    printf("Total: %lu in human form\n", totalmem);
+    printf("Used:  %lu in human form\n", usedmem);
 
     return 0;
 }
